ed0c3ad14014531c87235fdba3a4033b3f45f8f8
Amartya Saran <amartyasaran@gmail.com>
2024-05-30 19:17:04 +0530

    '

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import json
+import pickle
 import streamlit as st
-from haystack.document_stores import FAISSDocumentStore
-# from haystack.document_stores import InMemoryDocumentStore
+# from haystack.document_stores import FAISSDocumentStore
+from haystack.document_stores import InMemoryDocumentStore
 from transformers import DPRContextEncoder, DPRContextEncoderTokenizer
 from haystack.nodes import DensePassageRetriever
 from haystack.nodes import FARMReader
@@ -22,11 +23,12 @@ st.title("DPR on Supreme Court Judgements (Capital Gain)")
 #     } for doc in data
 # ]
 
-# document_store = FAISSDocumentStore(embedding_dim=768, faiss_index_factory_str="Flat")
-# document_store = InMemoryDocumentStore()
+# document_store = FAISSDocumentStore(embedding_dim=768, faiss_index_factory_str="Flat", sql_url="sqlite:///faiss_document_store.d")
+with open("inmemory_document_store.pkl", "rb") as f:
+    document_store = pickle.load(f)
 # document_store.write_documents(documents)
 
-document_store = FAISSDocumentStore.load("faiss_index")
+# document_store = FAISSDocumentStore.load(index_path="./faiss_index", config_path="./faiss_index.json")
 
 retriever = DensePassageRetriever(
     document_store=document_store,
@@ -35,29 +37,25 @@ retriever = DensePassageRetriever(
 )
 
 # document_store.update_embeddings(retriever)
-# document_store.save("./document_store")
-
+# document_store.save(index_path="./faiss_index", config_path="./faiss_index.json")
+# with open("inmemory_document_store.pkl", "wb") as f:
+#     pickle.dump(document_store, f)
 
 
 reader = FARMReader(model_name_or_path="deepset/bert-base-cased-squad2")
 
 pipeline = ExtractiveQAPipeline(reader=reader, retriever=retriever)
 
-# query = st.text_input("Enter your query:", "")
+query = st.text_input("Enter your query:", "")
 
-# # if query:
-# #     with st.spinner("Searching..."):
-# #         results = pipeline.run(query=query, params={"Retriever": {"top_k": 5}})
-# #         st.write("Results:")
-# #         for idx, result in enumerate(results["documents"]):
-# #             st.write(f"**{idx + 1}. {result.meta['name']}**")
-# #             st.write(f"URL: {result.meta['url']}")
-# #             st.write(result.content)
-# #             st.write("---")
+if query:
+    with st.spinner("Searching..."):
+        results = pipeline.run(query=query, params={"Retriever": {"top_k": 5}})
+        for answer in results['answers']:
+            st.markdown(f"=====================\nAnswer: {answer.answer}\nContext: {answer.context}\nScore: {answer.score}")
 
 # query = st.text_input("Enter Question")
-query = "What is the subject matter of the petition in the Sadanand S. Varde case?"
-result = pipeline.run(query=query, params={"Retriever": {"top_k": 10}, "Reader": {"top_k": 5}})
-
-for answer in result['answers']:
-    st.markdown(f"=====================\nAnswer: {answer.answer}\nContext: {answer.context}\nScore: {answer.score}")
+# query = "What is the subject matter of the petition in the Sadanand S. Varde case?"
+# result = pipeline.run(query=query, params={"Retriever": {"top_k": 10}, "Reader": {"top_k": 5}})
+# for answer in result['answers']:
+#    print(f"=====================\nAnswer: {answer.answer}\nContext: {answer.context}\nScore: {answer.score}")
