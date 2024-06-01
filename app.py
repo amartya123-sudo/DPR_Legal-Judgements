9e9b87650e4b07a986c74c226ffb08917c495bba
Amartya Saran <amartyasaran@gmail.com>
2024-06-01 12:54:22 +0530

    '

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import json
+import os
 import pickle
 import streamlit as st
 from haystack.nodes import DensePassageRetriever
@@ -7,37 +7,21 @@ from haystack.pipelines import ExtractiveQAPipeline
 
 st.title("DPR on Supreme Court Judgements (Capital Gain)")
 
-# with open("responses.json", 'r') as f:
-#   data = json.load(f)
+# Get the current directory of the script
+current_dir = os.path.dirname(__file__)
 
-# documents = [
-#     {
-#         "content": doc["text"],
-#         "meta": {
-#             "name": doc["title"],
-#             "url": doc["url"]
-#         }
-#     } for doc in data
-# ]
+# Construct the full path to the file
+file_path = os.path.join(current_dir, "inmemory_document_store.pkl")
 
-# document_store = FAISSDocumentStore(embedding_dim=768, faiss_index_factory_str="Flat", sql_url="sqlite:///faiss_document_store.d")
-with open("./inmemory_document_store.pkl", "rb") as f:
+# Open the file
+with open(file_path, "rb") as f:
     document_store = pickle.load(f)
-# document_store.write_documents(documents)
-
-# document_store = FAISSDocumentStore.load(index_path="./faiss_index", config_path="./faiss_index.json")
 
 retriever = DensePassageRetriever(
     document_store=document_store,
     query_embedding_model="facebook/dpr-question_encoder-single-nq-base",
     passage_embedding_model="facebook/dpr-ctx_encoder-single-nq-base",
 )
-
-# document_store.update_embeddings(retriever)
-# document_store.save(index_path="./faiss_index", config_path="./faiss_index.json")
-# with open("inmemory_document_store.pkl", "wb") as f:
-#     pickle.dump(document_store, f)
-
 
 reader = FARMReader(model_name_or_path="deepset/bert-base-cased-squad2")
 
@@ -50,9 +34,3 @@ if query:
         results = pipeline.run(query=query, params={"Retriever": {"top_k": 5}})
         for answer in results['answers']:
             st.markdown(f"=====================\nAnswer: {answer.answer}\nContext: {answer.context}\nScore: {answer.score}")
-
-# query = st.text_input("Enter Question")
-# query = "What is the subject matter of the petition in the Sadanand S. Varde case?"
-# result = pipeline.run(query=query, params={"Retriever": {"top_k": 10}, "Reader": {"top_k": 5}})
-# for answer in result['answers']:
-#    print(f"=====================\nAnswer: {answer.answer}\nContext: {answer.context}\nScore: {answer.score}")
